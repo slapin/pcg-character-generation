32f54119edf1ecb5f3c11856e2dc88d6ae3a626d
Sergey Lapin <slapinid@gmail.com>
2018-08-24 23:46:10 +0300

    pre-work setup complete

---
+++ main.gd
@@ -0,0 +1,33 @@
+extends Spatial
+
+var base
+var female
+var male
+var img1
+var img2
+
+func calc_difference(img, base_mesh, target_mesh):
+	img.lock()
+	for x in range(img.get_width()):
+		for y in range(img.get_height()):
+			var u = float(x)/img.get_width()
+			var v = float(y)/img.get_height()
+			img.set_pixel(x, y, Color(randf(), randf(), randf(), 1))
+	img.unlock()
+
+func _ready():
+	var dir = Directory.new()
+	dir.remove("res://assets/base/female.png")
+	dir.remove("res://assets/base/male.png")
+	img1 = Image.new()
+	img1.create(1024, 1024, false, Image.FORMAT_RGB8)
+	calc_difference(img1, null, null)
+	img1.save_png("res://assets/base/female.png")
+	img2 = Image.new()
+	img2.create(1024, 1024, false, Image.FORMAT_RGB8)
+	calc_difference(img2, null, null)
+	img2.save_png("res://assets/base/male.png")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
